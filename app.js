ae113cfa35b4dd8a86e8628fbcc60097f0255ffc
peter <pfranciskoe@gmail.com>
2020-07-29 09:53:47 -0700

    setup mongodb server

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const {graphqlHTTP} = require('express-graphql');
+const schema = require('./schema/schema')
+const app = express();
+const db = require('./config/keys').mongoURI;
+
+const mongoose = require('mongoose');
+
+mongoose.connect(db);
+
+mongoose.connection.once('open', () => {
+    console.log('conneted to database');
+});
+
+//This route will be used as an endpoint to interact with Graphql, 
+//All queries will go through this route. 
+app.use('/graphql', graphqlHTTP({
+    //directing express-graphql to use this schema to map out the graph 
+    schema,
+    //directing express-graphql to use graphiql when goto '/graphql' address in the browser
+    //which provides an interface to make GraphQl queries
+    graphiql: true
+}));
+
+app.listen(3000, () => {
+    console.log('Listening on port 3000');
+}); 
